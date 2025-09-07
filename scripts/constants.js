e92529c78bd376ad0e07c1184c545fc44f6d6879
Mingotrees <jhanell.mingo@gmail.com>
2025-09-07 11:14:45 +0800

    feat: login successful

--- scripts/constants.js
+++ scripts/constants.js
@@ -1,5 +1,5 @@
 let USER = localStorage.getItem('user');
-export const BASE_URL = "http://127.0.0.1:3000/";
+export const BASE_URL = "http://127.0.0.1:3000";
 export const API = "https://todo-list.dcism.org";
 
 export function getUser() {
@@ -9,4 +9,9 @@ export function getUser() {
 export function setUser(user) {
     USER = user;
     localStorage.setItem('user',  JSON.stringify(user));
+}
+
+export function logOut(){
+    localStorage.removeItem('user');
+    USER = null;
 }
--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,25 @@
-import {BASE_URL, getUser, setUser} from './constants.js';
+import {BASE_URL, getUser, logOut} from './constants.js';
+
+const logout = $("#logout-btn");
+
 
 let user = null;
+
+/*
+* Check if user exist in localStorage
+*/
 document.addEventListener("DOMContentLoaded", ()=>{
     if(!getUser()){
         alert("Login First Cousin");
-        window.location.replace(`${BASE_URL}login.html`);
+        window.location.replace(`${BASE_URL}/login.html`);
     }else{
         user = JSON.parse(getUser());
     }
 });
+
+logout.on('click', ()=>{
+    logOut();
+    window.location.replace(`${BASE_URL}/login.html`);
+})
+
+
